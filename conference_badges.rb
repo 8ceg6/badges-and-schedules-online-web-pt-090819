0c6dbb04ba3cbfcdffad7610e4c5d3be00a56a87
cedric e goddard <8ceg6@comcast.net>
2019-09-26 14:33:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+def badge_maker(attendees)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator
+  return "Hello, #{name}! You'll be assigned to room #{index}!"
+  
