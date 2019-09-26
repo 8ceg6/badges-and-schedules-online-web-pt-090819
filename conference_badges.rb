250270b35e8a376ad4872c165512798479e87394
cedric e goddard <8ceg6@comcast.net>
2019-09-26 14:48:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,12 @@
 # Write your code here.
-def badge_maker(name)
+def badge_maker(attendees)
 return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
+    attendees.collect do |attendee|
+      badge_maker(attendee)
+    end
+end
  
   
