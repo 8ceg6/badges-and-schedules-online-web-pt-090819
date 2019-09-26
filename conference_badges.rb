d770aa34a8549d8c2b0cab258c656ae143af64f9
cedric e goddard <8ceg6@comcast.net>
2019-09-26 15:40:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,8 +10,8 @@ def batch_badge_creator(attendees)
 end
  
   def assign_rooms (attendees)
-  attendees.map.with_index do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index  +1}!"
+  attendees.map.with_index(1) do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
 
 end
